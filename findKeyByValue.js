81c51121434a3872b599bc0c880c4911e6d42fb2
Racingwind <louis.k.hu@gmail.com>
2023-10-27 00:21:20 -0700

    added findKeyByValue.js file

---
+++ findKeyByValue.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+/* FUNCTION IMPLEMENTATION
+Prints success if two inputs exactly match each other. Fail otherwise
+
+:param actual: thing to be compared
+:param expected: thing to be compared to
+:type actual: simple data types
+:type expected: simple data types
+:return: does not return anything
+*/
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed : ${actual} ===  ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(inputList, showName) {
+  /*finds the key (genre) of the given input value (show name)
+  :param inputList: object with keys as genre and only 1 value for each key as show
+  :param showName: name of the show to look for
+  :type inputList: object
+  :type showName: string
+  :return string: name of the key
+  */
+  for (const genre in inputList) {
+    if (inputList[genre] === showName) {
+      return genre;
+    }
+  }
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
