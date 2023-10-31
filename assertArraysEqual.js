69e20cb10e4f148767ae180514d84591ce9f65a9
Racingwind <louis.k.hu@gmail.com>
2023-10-31 15:19:07 -0700

    added map.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -36,6 +36,12 @@ const assertArraysEqual = function(input1, input2) {
 };
 
 // TEST CODE
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertArraysEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+console.log([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+console.log([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+console.log(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+console.log(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
