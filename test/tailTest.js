de6f36074a9690255025c50e60760112da02d631
Racingwind <louis.k.hu@gmail.com>
2023-11-11 02:37:16 -0800

    refactored middleTest.js to use mocha and chai. cleaned up spacing in tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const tail   = require('../tail');
+const tail = require('../tail');
 
 describe("#tail", () => {
   it("returns [2, [3, 4]] for [1, 2, [3, 4]]", () => {
