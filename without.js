015e79622fc53d91f03936a434d05b2d839923c5
Racingwind <louis.k.hu@gmail.com>
2023-11-11 04:16:17 -0800

    imported assertArraysEqual and exported without

--- without.js
+++ without.js
@@ -1,39 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  /*Returns true if both input arrays match each other by element. false otherwise
-  :param arr1: array 1
-  :param arr2: array 2
-  :type arr1: array
-  :type arr2: array
-  :return: boolean
-  */
-  let arrLength = arr1.length;
-  if (arrLength === arr2.length) {
-    for (let i = 0; i < arrLength; i++) {
-      if (arr1[i] !== arr2[i]) { //if we find an element to not match, immediately return with false
-        return false;
-      }
-    }
-    return true; //all elements passed, return true
-  }
-  else {
-    return false; //array length does not match, they cannot be equal
-  }
-};
-
-const assertArraysEqual = function(input1, input2) {
-  /*Prints success if two inputs exactly match each other. Fail otherwise
-  :param input1: thing to be compared
-  :param input2: thing to be compared to
-  :type input1: array
-  :type input2: array
-  :return: does not return anything
-  */
-  if (eqArrays(input1, input2) === true) {
-    console.log(`✅ Assertion Passed : ${input1} ===  ${input2}`);
-  } else {
-    console.log(`❌ Assertion Failed : ${input1} !== ${input2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const without = function(arr, values) {
   /*without removes a given list of values from the input array and returns a new array
@@ -63,6 +28,8 @@ const without = function(arr, values) {
 
   return arrLocal;
 };
+
+module.export = without;
 
 console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
