c7c3afbfe955d76b923248ae09e983a8b615bc1e
Racingwind <louis.k.hu@gmail.com>
2023-11-11 01:58:30 -0800

    refactored tailTest.js to use mocha and chai

--- tail.js
+++ tail.js
@@ -1,7 +1,7 @@
 /*tail returns the a given array without the first element. if the array has a length of 1 or it's empty, it returns an empty array
 :param arr: array to be worked with
 :type arr: array
-:return: values of first element of array, or undefined
+:return: values of the input array without its first element
 */
 const tail = function(arr) {
   return (arr.length !== 0) ? arr.slice(1) : arr; //if array length is 0, it's empty and return itself, which is empty
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,27 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("returns [2, [3, 4]] for [1, 2, [3, 4]]", () => {
+    assert.deepEqual(tail([1, 2, [3, 4]]), [2, [3, 4]]);
+  });
 
-let array = [1, 2, 3];
-assertEqual(array.length, 3);
+  it("returns [[3, 4]] for [[1, 2], [3, 4]]", () => {
+    assert.deepEqual(tail([[1, 2], [3, 4]]), [[3, 4]]);
+  });
+
+  it("returns '[]' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+
+  it("returns '[]' for [undefined]", () => {
+    assert.deepEqual(tail([undefined]), []); 
+  });
+
+  it("returns '[Lighthouse, Labs]' for [Hello, Lighthouse, Labs] without changing the original", () => {
+    const arrTest = ["Hello", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(arrTest), ["Lighthouse", "Labs"]);
+    assert.deepEqual(arrTest, ["Hello", "Lighthouse", "Labs"]);
+  });
+
+});
