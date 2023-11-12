80715a9e6b82fee282cc14091b85c872c0e51750
Racingwind <louis.k.hu@gmail.com>
2023-11-11 23:47:32 -0800

    added extra test case to check for nested arrays more than 3 levels deep

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -17,4 +17,7 @@ assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
 
 assertEqual(eqArrays([[2, 3], [4], [5, [6, 7]]], [[2, 3], [4], [5, [6, 7]]]), true)
 assertEqual(eqArrays([[2, 3], [4], [5, [6, 7]]], [[2, 3], [4], [5, [6, 8]]]), false)
+
+assertEqual(eqArrays([[2, 3], [4], [5, [6, 7, [8, 9, [10, undefined], 11], 12]]], [[2, 3], [4], [5, [6, 7, [8, 9, [10, undefined], 11], 12]]]), true)
+assertEqual(eqArrays([[2, 3], [4], [5, [6, 7, [8, 9, [10, null], 11], 12]]], [[2, 3], [4], [5, [6, 7, [8, 9, [10, undefined], 11], 12]]]), false)
 console.log("-----");
