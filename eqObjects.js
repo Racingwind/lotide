c3bbb03287870a14343738ff4f84e91a7c31c0eb
Racingwind <louis.k.hu@gmail.com>
2023-11-11 23:08:53 -0800

    fixed a bug in which the function does not check if the keys match. updated it to work with nested objects. included additional tests to test nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,41 @@
 const eqArrays = require('./eqArray')
 
 const eqObjects = function(object1, object2) {
-/*Returns true if both objects have identical keys with identical values. Returns false otherwise.
-:param object1: the object to inspect
-:param object2: the object to compare to
-:type object1: object
-:type object2: object
-:return: boolean
-*/
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (const key in object1) {
-      let objKey1 = object1[key];
-      let objKey2 = object2[key];
-      if (Array.isArray(objKey1) && Array.isArray(objKey2)) {
-        if (!(eqArrays(objKey1, objKey2))) {
-          return false;
-        }
-      } else {
-        if (objKey1 !== objKey2) {
-          return false;
-        }
-      }
-    }
-    return true;
-  } else {
+  /*Returns true if both objects have identical keys with identical values. Returns false otherwise.
+  :param object1: the object to inspect
+  :param object2: the object to compare to
+  :type object1: object
+  :type object2: object
+  :return: boolean
+  */
+
+  arrKeys1 = Object.keys(object1);
+  arrKeys2 = Object.keys(object2);
+  match = (arrKeys1.every(x => arrKeys2.includes(x)))
+  if (!match || arrKeys1.length !== arrKeys2.length) { // keys do not match if they don't contain each other or their lengths are different
     return false;
   }
+  for (const key of arrKeys1) {
+    console.log("check key")
+    value1 = object1[key];
+    value2 = object2[key];
+    if ((Object.prototype.toString.call(object1[key]) === "[object Object]") && (Object.prototype.toString.call(object2[key]) === "[object Object]")) { // values are objects
+      if (!eqObjects(object1[key], object2[key])) {
+        return false;
+      }
+      break; // the two values match, skip the rest of the check
+    }
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // values are arrays
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+      break; // the two values match, skip the rest of the check
+    }
+    if (value1 !== value2) { // values are primitives
+      return false;
+    }
+  }
+  return true; // all checks passed
 };
 
 module.exports = eqObjects;
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -2,6 +2,11 @@ const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
 describe("#eqObjects", () => {
+
+  it("returns true for { color: 'red', size: 'medium' } and { color: 'red', size: 'medium' }", () => {
+    assert.strictEqual(eqObjects({ color: "red", size: "medium" }, { color: "red", size: "medium" }), true);
+  });
+  
   it("returns true for { color: 'red', size: 'medium' } and { size: 'medium', color: 'red' }", () => {
     assert.strictEqual(eqObjects({ color: "red", size: "medium" }, { size: "medium", color: "red" }), true);
   });
@@ -14,12 +19,36 @@ describe("#eqObjects", () => {
     assert.strictEqual(eqObjects({ colors: ["red", "blue"], size: "medium" }, { size: "medium", colors: ["red", "blue"] }), true);
   });
 
+  it("returns false for { colors: ['blue', 'red'], size: 'medium' } and { size: 'medium', colors: ['red', 'blue'] }", () => {
+    assert.strictEqual(eqObjects({ colors: ["blue", "red"], size: "medium" }, { size: "medium", colors: ["red", "blue"] }), false);
+  });
+
+  it("returns false for { colors: ['red', 'blue', 'green'], size: 'medium' } and { size: 'medium', colors: ['red', 'blue'] }", () => {
+    assert.strictEqual(eqObjects({ colors: ["blue", "red", "green"], size: "medium" }, { size: "medium", colors: ["red", "blue"] }), false);
+  });
+
+  it("returns false for { colors: ['red', 'blue'], size: 'medium' } and { size: 'medium', colors: ['red', 'blue', 'green'] }", () => {
+    assert.strictEqual(eqObjects({ colors: ["blue", "red"], size: "medium" }, { size: "medium", colors: ["red", "blue", "green"] }), false);
+  });
+
   it("returns false for { colors: ['red', 'blue'], size: 'medium' } and { size: 'medium', colors: ['red', 'blue'], sleeveLength: 'long' }", () => {
     assert.strictEqual(eqObjects({ colors: ["red", "blue"], size: "medium" }, { size: "medium", colors: ["red", "blue"], sleeveLength: "long" }), false);
   });
 
   it("returns false for { colors: ['r', 'e', 'd'], size: 'medium' } and { size: 'medium', colors: 'red' }", () => {
     assert.strictEqual(eqObjects({ colors: ["r", "e", "d"], size: "medium" }, { size: "medium", colors: "red" }), false);
+  });
+
+  it("returns false for { a: { z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }", () => {
+    assert.strictEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  });
+
+  it("returns false for { a: { y: 0, z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }", () => {
+    assert.strictEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  });
+
+  it("returns false for { a: { y: 0, z: 1 }, b: 2 } and { a: 1, b: 2 }", () => {
+    assert.strictEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
   });
 
 });
